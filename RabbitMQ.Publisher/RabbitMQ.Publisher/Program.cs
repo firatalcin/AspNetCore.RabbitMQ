d46f05208e619c3747d2fa06781dadd3a94a0edd
Fırat Alçın <firatalcin04@gmail.com>
2023-05-28 22:12:03 +0300

    ...

---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,8 @@
+﻿using RabbitMQ.Client;
+
+// Bağlantı Oluşturma (CloudAMQP üzerinden bağlantı)
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqps://tjoxyejw:aUyOaBIKUE6cdOvfFJvQhkIS16icNkmE@woodpecker.rmq.cloudamqp.com/tjoxyejw");
+
+//Bağlantıyı Aktifleştirme ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
